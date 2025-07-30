34c3585ac5604196000cf8b08328335976650050
Șerban Carole <s.carole@student.tudelft.nl>
2025-07-30 21:21:41 +0300

    reijgouirhg

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const butonas = document.getElementById("buton");
 const lista = document.getElementById("lista");
 const input = document.getElementById("tb");
+const raspuns = document.getElementById('raspuns');
+let raspunsuri = ['Decent guess','Doamne pe bune?','No comment','Ok bun guess'];
 butonas.addEventListener("click",apasat);
 function apasat(){
 	let text = input.value;
-	if(text === "Susu Arthur" || text === "susu arthur"){
-		let sigma = document.createElement('h3');
-		sigma.appendChild(document.createTextNode('Chiar credeai'));
-		document.getElementById('total').appendChild(sigma);
-	}
 	if(text === "GNARF"){
-		alert("Bravo, ai gasit cuvantul secret! Respect.")
+		alert("Bravo, ai gasit cuvantul secret! Respect.");
+		raspuns.innerText = 'Felicitari';
+		let x = document.createElement("li");
+		x.appendChild(document.createTextNode(text));
+		lista.appendChild(x);
+		input.value = '';
+	}else if(text === "Susu Arthur" || text === "susu arthur"){
+		raspuns.innerText = 'Chiar credeai';
+		let x = document.createElement("li");
+		x.appendChild(document.createTextNode(text));
+		lista.appendChild(x);
+		input.value = '';
+	}else {
+		let x = document.createElement("li");
+		x.appendChild(document.createTextNode(text));
+		lista.appendChild(x);
+		input.value = '';
+		raspuns.innerText = raspunsuri[Math.floor(Math.random()*4)];
 	}
-	let x = document.createElement("li");
-	x.appendChild(document.createTextNode(text));
-	lista.appendChild(x);
-	input.value = '';
+	
 }
